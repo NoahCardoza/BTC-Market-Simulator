b08b5e5712d8d49de77cf42556ca66b7d0d8a2dc
Noah Cardoza <noahcardoza@gmail.com>
2018-07-03 22:19:06 -0700

    Added an HTML page to signup.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const path = require('path');
 const database = require('./database');
 
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const app = express();
+
 app.use(morgan('dev'))
 app.use(bodyParser.json())
+app.use('/', express.static(path.resolve(__dirname, 'static')))
 
 database.start().then(db => {
   require('./routes')(app, db)
