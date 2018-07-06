c1e804b5a967e19fbdcda786fddb2c52a08fc738
Noah Cardoza <noahcardoza@gmail.com>
2018-07-06 09:53:21 -0700

    fixed header issue

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.resolve(__dirname, 'static')))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization')
+  if (req.headers.origin) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization')
+  }
   next()
 })
 
